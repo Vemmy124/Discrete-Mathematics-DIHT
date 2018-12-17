1932eeeac0c05d50eac903ee31259315ba8f4fae
Vemmy124 <vemmy124@gmail.com>
2018-12-17 14:22:30 +0300

    removed random graph option

--- Discrete-Analysis/Graph-Coloring/GreedyGraphColoring.py
+++ Discrete-Analysis/Graph-Coloring/GreedyGraphColoring.py
@@ -3,7 +3,6 @@ from random import shuffle
 from math import *
 
 def gen_graph(n, p):
-    p = eval(p)
     graph = [[True if j > i and uniform(0, 1) < p else False for j in range(n)] for i in range(n)]
     for i in range(n):
         for j in range(i):
@@ -25,11 +24,11 @@ def greedy_graph_color(graph):
     colors_count = 0
     coloring = [-1] * len(graph)
     shuffled_vertices = list(range(len(graph)))
-    shuffle(shuffled_vertices)
+#   shuffle(shuffled_vertices)
     for vertex in shuffled_vertices:
         used_colors = [False] * len(graph)
         for successor in range(len(graph)):
-            if graph[successor][vertex] and coloring[successor] != -1:
+            if graph[vertex][successor] and coloring[successor] != -1:
                 used_colors[coloring[successor]] = True
         for color in range(len(coloring)):
             if not used_colors[color]:
@@ -39,15 +38,19 @@ def greedy_graph_color(graph):
     return coloring, colors_count
 
 
-input_fmt = int(input("Enter a number:\n1)Random graph\n2)Custom graph\n"))
+#input_fmt = int(input("Enter a number:\n1)Random graph\n2)Custom graph\n"))
 n = int(input("Enter number of vertices n:\n"))
 graph = []
-if input_fmt == 1:
-    p = input("Enter the probability of an edge (python arithmetic expression, may be depending on n):\n")
-    graph = gen_graph(n, p)
-elif input_fmt == 2:
-    print("Enter an undirected graph (adjacency matrix, 1 if there is an edge, 0 else):")
-    for i in range(n):
+#if input_fmt == 1:
+#    p = input("Enter the probability of an edge p (python arithmetic expression, may be depending on n):\n")
+#    try:
+#        p = eval(p)
+#    except:
+#        print("Incorrect probability expression")
+#    graph = gen_graph(n, p)
+#elif input_fmt == 2:
+print("Enter an undirected graph (adjacency matrix, 1 if there is an edge, 0 else):")
+for i in range(n):
         graph.append(list(map(lambda x: bool(int(x)), input().split())))
 if not is_undirected(graph):
     print("Incorrect graph")
@@ -56,4 +59,4 @@ coloring, colors_count = greedy_graph_color(graph)
 print("\nNumber of colors returned by the greedy algorithm:\n", colors_count, sep='')
 print("Vertex: color")
 for item in ((i + 1, coloring[i] + 1) for i in range(n)):
-    print("{}: {}".format(*item))
+    print("{}: {}; ".format(*item), end='')
